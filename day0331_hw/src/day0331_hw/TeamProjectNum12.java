b64793656b3ae9c9847ecee4eb37c8d5a97e52ee
yanapang <yanahwang@gmail.com>
2022-04-05 09:55:39 +0900

    4th day of studying java(part.2)

---
+++ day0331_hw/src/day0331_hw/TeamProjectNum12.java
@@ -0,0 +1,26 @@
+package day0331_hw;
+
+import java.util.Scanner;
+
+public class TeamProjectNum12 {
+
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		//-1 입력 받기 전까지 계속 입력 받아 배열에저장.
+//		//배열 출력
+//		//sum & avg 출력
+//		System.out.println("값을 입력하세요: ");
+//		int cnt = 0;
+//		int arr[] = new int[]; //arr 생성.
+//		
+//		
+//		for (int i=0; ;i++) {
+//			if (input_numb == -1) {
+//				break;
+//			}
+//		}
+//		System.out.println(i);
+		
+	}
+
+}
