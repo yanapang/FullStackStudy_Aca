f7c11f337512f389d40a04eac71d1027cf3232fc
Yana Hwang <46346904+yanapang@users.noreply.github.com>
2022-06-21 22:19:40 +0900

    Create MemberManager.java
    DB Manager.

---
+++ day0621_SpringSecurity/MemberManager.java
@@ -0,0 +1,37 @@
+package com.example.demo.db;
+
+public class MemberManager {
+
+	public static SqlSessionFactory sqlSessionFactory;
+	static {
+		
+		try {
+			String resource = "com/example/demo/db/sqlMapConfig.xml";
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory =
+			  new SqlSessionFactoryBuilder().build(inputStream);
+			
+		} catch(Exception e) {
+			System.out.println("Error : "+ e.getMessage());
+		}
+	}
+	
+	
+	public static int insert(MemberVO m) {
+		int re=-1;
+		SqlSession session = sqlSessionFactory.openSession();
+		re = session.insert("member.insert", m);
+		session.commit();
+		session.close();
+		return re;
+	}
+	
+	
+	public static MemberVO findById(String id) {
+		SqlSession session = sqlSessionFactory.openSession();
+		MemberVO m = session.selectOne("member.findById", id);
+		session.close();
+		return m;
+	}
+	
+}
