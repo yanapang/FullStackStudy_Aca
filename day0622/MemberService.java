a0aacbe8fef4912f1f50441f4385ca4da38f0520
Yana Hwang <46346904+yanapang@users.noreply.github.com>
2022-06-22 22:23:12 +0900

    user detail service class
    user detail service class

---
+++ day0622/MemberService.java
@@ -0,0 +1,30 @@
+
+@Service
+@Setter
+public class MemberService implements UserDetailsService {
+
+	@Autowired
+	private MemberDAO dao;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		System.out.println("Processing LogIn");
+		
+		MemberVO m = dao.findById(username);
+		if(m == null) {//Return Error when input username is invalid
+			throw new UsernameNotFoundException(username);
+		}
+		
+		//returns User Entity, if valid user
+		UserDetails user = User.builder()
+				.username(username)
+				.password(m.getPwd())
+				.roles(m.getRole())
+				.build();
+		
+		
+		return user;
+	}
+
+}
